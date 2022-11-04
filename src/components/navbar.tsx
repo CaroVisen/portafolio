ac50c38fc1173d47c1451ecd6979ff79664ff91c
CaroVisen <carolinaaylen26@gmail.com>
2022-11-04 02:02:08 -0300

    agrego mas cosas

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,35 +3,30 @@ import React from "react";
 const Navbar = ({
 
 }) => {
-    return(
-        <div style={{backgroundColor: '#171718', height:'75px', position:'sticky', color:'#F00EA6', display:'flex', justifyContent:'space-between', alignItems:'center', padding: '0 5%'}}>
-            <div style={{width:'10%'}}>
+    return (
+        <div className="nav" style={{ backgroundColor: '#171718', height: '75px', position: 'sticky', color: '#47D16E', display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 5%' }} id='nav'>
+            <div style={{ width: '10%' }}>
                 <h2>Carolina Visentin</h2>
             </div>
             <ul className="menu">
                 <li>
-                    <a href="#0">
-                    Inicio
+                    <a href="#nav">
+                        Inicio
+                    </a>
+                </li>
+                <li>
+                    <a href="#inicio">
+                        Acerca de mi
+                    </a>
+                </li>
+                <li>
+                    <a href="#abilities">
+                        Habilidades
                     </a>
                 </li>
                 <li>
                     <a href="#0">
-                    Acerca de mi
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                    Habilidades
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                    Experiencia
-                    </a>
-                </li>
-                <li>
-                    <a href="#0">
-                    Proyectos
+                        Proyectos
                     </a>
                 </li>
             </ul>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "../components/navbar.tsx";
+import Inicio from "../components/inicio.tsx";
+import Abilities from "../components/abilities.tsx";
+import Projects from "../components/projects.tsx";
+
 
 const Home = ({
 
 }) => {
-    return(
+    return (
         <div>
-            <Navbar/>
+            <Navbar />
+            <Inicio />
+            <Abilities />
+            <Projects />
         </div>
     )
 }
